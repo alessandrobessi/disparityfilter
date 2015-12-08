f667b905efa17a55b589b032876489084b38202e
François <briatte@gmail.com>
2015-12-08 14:45:20 +0100

    update disparity filter (directed networks, igraph integration)

--- R/disparity_filter.R
+++ R/disparity_filter.R
@@ -1,74 +1,93 @@
-# DISPARITY FILTER
-#' Disparity Filter Algorithm of Weighted Network
+#' Extract the backbone of a weighted network using the disparity filter
 #'
-#' Disparity filter is a network reduction algorithm to extract the backbone structure of both directed and undirected weighted networks. Disparity filter can reduce the network without destroying the multi-scale nature of the network. The algorithm has been developed by M. Angeles Serrano, Marian Boguna, and Alessandro Vespignani in "Extracting the multiscale backbone of complex weighted networks", Proceedings of the National Academy of Sciences 106 (16).
-#' @param graph igraph graph object. The original weighted graph.
-#' @param alpha Statistical significance level. By default is set to 0.05.
-#' @param directed Logical, whether the network is directed or undirected. By default is set to FALSE.
-#' @keywords backbone extraction disparity filter
-#' @import igraph
-#' @export get.backbone
+#' Given a weighted graph, \code{backbone} identifies the 'backbone structure'
+#' of the graph, using the disparity filter algorithm by Serrano et al. (2009).
+#' @param graph The input graph.
+#' @param weights A numeric vector of edge weights, which defaults to
+#' \code{E(graph)$weight}.
+#' @param directed The directedness of the graph, which defaults to the result
+#' of \code{\link[igraph]{is_directed}}.
+#' @param alpha The significance level under which to preserve the edges, which
+#' defaults to \code{0.05}.
+#' @return An edge list corresponding to the 'backbone' of the graph, i.e. the
+#' edges of the initial graph that were preserved by the null model that the
+#' disparity filter algorithm implements.
+#' @author Serrano et al. (2009); R implementation by Alessandro Bessi and
+#' Francois Briatte
+#' @references Serrano, M.A., Boguna, M. and Vespignani, A. (2009).
+#' Extracting the multiscale backbone of complex weighted networks.
+#' \emph{Proceedings of the National Academy of Sciences} 106, 6483-6488.
 #' @examples
-#' head(network)
-#' G = graph.data.frame(network, directed = FALSE)
-#' G_backbone = get.backbone(graph = G, alpha = 0.05, directed = FALSE)
+#' # undirected network
+#' g <- sample_pa(n = 250, m = 5, directed = FALSE)
+#' E(g)$weight <- sample(1:25, ecount(g), replace = TRUE)
+#' backbone(g)
+#'
+#' # directed network
+#' g <- sample_pa(n = 250, m = 5, directed = TRUE)
+#' E(g)$weight <- sample(1:25, ecount(g), replace = TRUE)
+#' backbone(g)
+#' @importFrom igraph as_data_frame degree E ego is_directed is_igraph
+#' @export
+backbone <- function(graph, weights = E(graph)$weight,
+                     directed = is_directed(graph), alpha = 0.05) {
 
-get.backbone = function(graph, alpha = 0.05, directed = FALSE)
-{
-  G = graph
-
-  # get edgelist
-  edgelist = get.data.frame(G)
-  colnames(edgelist) = c("from","to","weight")
-
-  # get nodes list
-  nodes = unique(c(edgelist[,1], edgelist[,2]))
-  N = length(nodes)
-
-  # initialize backbone dataframe
-  backbone = NULL
-
-  cat("Disparity Filter\n")
-  cat("alpha =", alpha, "\n")
-  cat("\nOriginal graph\n")
-  print(G)
-
-  for (i in 1:N) # for each node
-  {
-    # get neighbors
-    nei = edgelist[edgelist$from == nodes[i],]
-    nei = rbind(nei, edgelist[edgelist$to == nodes[i],])
-
-    # get degree for node i
-    k_i = length(edgelist$to[edgelist$to == nodes[i]]) + length(edgelist$to[edgelist$from == nodes[i]])
-
-    if (k_i>1)
-    {
-      for (j in 1:k_i) # for each neighbor
-      {
-        # compute weighted edge
-        p_ij = as.numeric(nei$weight[j]) / sum(as.numeric(nei$weight))
-
-        # VIA INTEGRATION
-        #integrand_i = function(x){(1-x)^(k_i-2)}
-        #integration = integrate(integrand_i, lower = 0, upper = p_ij)
-        #alpha_ij = 1 - (k_i - 1) * integration$value
-
-        alpha_ij = (1 - p_ij)^(k_i - 1)
-
-        if (alpha_ij < alpha)
-        {
-          backbone = rbind(backbone, c(nei$from[j], nei$to[j], nei$weight[j]))
-        }
-      }
-    }
+  if (!is_igraph(graph)) {
+    stop("Not a graph object")
   }
 
-  colnames(backbone) = c("from","to","weight")
-  backbone = unique(backbone[,c('from','to','weight')])
-  G_backbone = graph.data.frame(backbone, directed = directed)
+  stopifnot(!is.null(weights))
 
-  cat("\nBackbone graph\n")
-  print(G_backbone)
-  return(G_backbone)
+  if (!directed) {
+    b = disparity_filter(graph, weights, "all", alpha)
+  } else {
+    b = rbind(
+      disparity_filter(graph, weights, "in", alpha),
+      disparity_filter(graph, weights, "out", alpha)
+    )
+  }
+  return(unique(b[ order(b$from), ]))
+
+}
+
+#' @keywords internal
+disparity_filter <- function(G, weights, mode = "all", alpha = 0.05) {
+
+  d = degree(G, mode = mode)
+  e = cbind(as_data_frame(G)[, 1:2 ], weight = weights, alpha = NA)
+  if (mode == "all") {
+    e = rbind(e, data.frame(from = e[, 2], to = e[, 1], e[, 3:4 ]))
+  }
+
+  for (u in which(d > 1)) {
+
+    w = switch(substr(mode, 1, 1),
+      a = which(e[, 1] == u | e[, 2] == u),
+      i = which(e[, 2] == u),
+      o = which(e[, 1] == u)
+    )
+    w = sum(e$weight[ w ]) / (1 + (mode == "all"))
+
+    k = d[u]
+
+    for (v in ego(G, 1, u, mode)[[1]][-1]) {
+
+      ij = switch(substr(mode, 1, 1),
+        a = which(e[, 1] == u & e[, 2] == v),
+        i = which(e[, 1] == v & e[, 2] == u),
+        o = which(e[, 1] == u & e[, 2] == v)
+      )
+      # cat(mode, "-", u, "->", v, ":", ij, "\n")
+
+      # p_ij = e$weight[ ij ] / w
+      # alpha_ij = integrate(function(x) { (1 - x) ^ (k - 2) }, 0, p_ij)
+      # alpha_ij = 1 - (k - 1) * alpha_ij$value
+      e$alpha[ ij ] = (1 - e$weight[ ij ] / w) ^ (k - 1)
+
+    }
+
+  }
+
+  return(e[ !is.na(e$alpha) & e$alpha < alpha, 1:4 ])
+
 }
